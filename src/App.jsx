ea3922ab642127f8e74fe99db0c78a564d329d4c
Coders <reprogdesenvolvimento@gmail.com>
2023-11-15 15:55:45 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import MeuComponente from './components/MeuComponente'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Olá mundo</h1>
+      <MeuComponente />
+      <MeuComponente />
+
+    </div>
+  )
+}
+
+
+
+
+
+export default App
---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,6 @@
+export default function MeuComponente() {
+    return (
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam voluptatibus, sapiente unde adipisci eos maiores cumque recusandae voluptate provident dolorum iste ipsum voluptatem enim, ullam nulla nostrum, dignissimos eligendi possimus.</p>
+    )
+}
+
